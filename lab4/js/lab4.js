a092469443bb0e5b6a26da32cee804e24f8f8593
Maya-ThePapaya <166079150+Maya-ThePapaya@users.noreply.github.com>
2024-04-22 16:19:26 -0700

    changed stuff

---
+++ lab4/js/lab4.js
@@ -0,0 +1,20 @@
+/**
+ * Lab 4 Rock Papaer Scisors list 
+ * 
+ * @author Maya Pahre and Harlow 
+ * @since  x.x.x
+ */
+
+//Set rules of the game: rock beats scissors, paper beats rock, and scissors beat paper
+//Get player input by prompting player to pick rock, paper, or scissors
+//Read player's input of rock, paper, or scissors
+//Generate a random number to represent the computer’s choice (1=rock, 2=paper, 3=scissors)
+//Convert random number into corresponding choice
+//Compare player’s choice with computer’s choice to determine winner
+//Implement game logic to determine outcome: rock beats scissors, paper beats rock, and scissors beat paper, if the same they tie
+//Display result of the game (you win, you lose, you tie)
+//Ask if they want to play again and repeat step 2-8 if they say yes
+
+
+
+
