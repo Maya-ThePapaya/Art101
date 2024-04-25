a461b0a9b621a732929940aaf65e5e304b93f4ed
Maya-ThePapaya <166079150+Maya-ThePapaya@users.noreply.github.com>
2024-04-25 16:15:48 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// * 
+//*
+//*
+ //*Toyota yaris Car Code
+ //* coding datatype and variables for Toyota yaris
+ //*
+ //* @author Harlow Knott and Maya Pahre
+//* @since April 2024
+ //* license: public domain 
 
-// Constants
+//Define Variables;
+make = "Toyota";
+model = "Yaris";
+color = "Red";
+year = "2018";
+ownIt = true;
 
-// Functions
+//calculate
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
+document.writeln("ownIt: " + ownIt + "<br>");
